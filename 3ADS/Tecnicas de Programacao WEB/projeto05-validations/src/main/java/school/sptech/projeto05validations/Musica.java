ecdf3ead3a4fe03eb46335f4161cd805487bbf86
Faariaas <gabriel.fsantos@sptech.school>
2023-03-28 20:05:55 -0300

    alguma c

--- 3ADS/Tecnicas de Programacao WEB/projeto05-validations/src/main/java/school/sptech/projeto05validations/Musica.java
+++ 3ADS/Tecnicas de Programacao WEB/projeto05-validations/src/main/java/school/sptech/projeto05validations/Musica.java
@@ -4,8 +4,10 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.validation.constraints.Min;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import java.time.LocalDate;
 
 @Entity
@@ -15,9 +17,12 @@ public class Musica {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
-    @NotNull
     @NotBlank
+    @Size(min = 3)
     private String nome;
+
+    @NotBlank
+    @Size(min = 3, max = 10)
     private String album;
     private LocalDate dataLancamento;
     private Double duracao;
