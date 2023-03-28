311b05fb48e1478f3a2dc176f7ffc939a9ae808f
Faariaas <gabriel.fsantos@sptech.school>
2023-03-28 19:58:47 -0300

    Test IntelJ

--- 3ADS/Tecnicas de Programacao WEB/projeto05-validations/src/main/java/school/sptech/projeto05validations/Musica.java
+++ 3ADS/Tecnicas de Programacao WEB/projeto05-validations/src/main/java/school/sptech/projeto05validations/Musica.java
@@ -4,6 +4,8 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import java.time.LocalDate;
 
 @Entity
@@ -12,6 +14,9 @@ public class Musica {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
+
+    @NotNull
+    @NotBlank
     private String nome;
     private String album;
     private LocalDate dataLancamento;
